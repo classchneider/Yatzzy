53c873966e1e95caabdc4bbe99849b389a738d8a
classchneider <classchneider@gmail.com>
2022-11-29 13:39:34 +0100

    Add project files.

---
+++ ViewModels/VMResult.cs
@@ -0,0 +1,40 @@
+﻿using YatzyRepository;
+
+namespace ViewModels
+{
+    public class VMResult
+    {
+        Result result;
+
+        public VMResult(Result res)
+        {
+            result = res;
+        }
+
+        public int id
+        {
+            get
+            {
+                return result.Id;
+            }
+        }
+        public int sixes
+        {
+            get { return result.sixes; }
+        }
+        
+        public int fives { get { return result.fives; } }
+        public int fours { get { return result.fours; } }
+        public int threes { get { return result.threes; } }
+        public int twos { get { return result.twos; } }
+        public int ones { get { return result.ones; } }
+
+        int Sum
+        {
+            get
+            {
+                return sixes + fives + fours + threes + twos + ones;
+            }
+        }
+    }
+}
---
+++ YatzyRepository/Model.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace YatzyRepository
+{
+    public class Model : DbContext
+    {
+        public DbSet<Player> Player { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EF_Yatzy; Trusted_Connection = True; ");
+        }
+    }
+
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Result
+    {
+        public int Id { get; set; }
+        public int sixes { get; set; }
+        public int fives { get; set; }
+        public int fours { get; set; }
+        public int threes { get; set; }
+        public int twos { get; set; }
+        public int ones { get; set; }
+    }
+}
